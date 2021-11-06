c2e19988c54ce710e88bf21efa88b13a003820c0
Victor Johnsson <victor.johnsson@gmail.com>
2021-11-06 13:47:45 +0100

    cleanup

--- test.py
+++
@@ -1,8 +0,0 @@
-text = " ，那地方也不通班车"
-text = text.replace("-", "一")
-text = text.replace("+", "十")
-text = text.replace("，", "");
-text = text.replace("。", "");
-text = text.replace("”", "");
-text = text.strip();
-print(text)
