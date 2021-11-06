2a2fcee006642f4c69f81c3db757541e8b3eea66
Victor Johnsson <victor.johnsson@gmail.com>
2021-11-06 11:10:16 +0100

    Now printing as srt file

--- start.py
+++ start.py
@@ -1,17 +1,15 @@
 from PIL import Image
 import pytesseract
 import imagehash
-import time
 import cv2
 import numpy as np
 
 
-def load_text_image(img_path):
-    im = Image.open(img_path)
-    cropped = im.crop((600, 1200, 2750, 1460))
-    imgrey = cropped.convert('L')
-    # turn white into black and grey and black into white
-    return imgrey.point(lambda value: 0 if value > 254 else 255)
+def millis_to_hhmmss(total_millis):
+    (total_seconds, millis) = divmod(total_millis, 1000)
+    (total_minutes, seconds) = divmod(total_seconds, 60)
+    (hours, minutes) = divmod(total_minutes, 60)
+    return '{:02}:{:02}:{:02},{:03}'.format(int(hours), int(minutes), int(seconds), int(millis))
 
 
 Video_FILE = '...'
@@ -23,18 +21,23 @@ if (cap.isOpened() == False):
     print("Error opening video stream or file")
 
 prev_hash = imagehash.hex_to_hash('0' * 256)
+subtitle_index = 1
+prev_subtitle = ""
+prev_change_millis = 0  # either the start or the end of a subtitle line
+
 while(cap.isOpened()):
     ret, frame = cap.read()
-    if ret == True:
-
+    if ret:
         cropped = frame[1600:2160, 820:3020]
         img = cv2.cvtColor(cropped, cv2.COLOR_BGR2GRAY)
         img = cv2.threshold(img, 250, 255, cv2.THRESH_BINARY)[1]
         above_subtitles = np.array([[0, 0], [0, 46], [2200, 46], [2200, 0]])
-        below_subtitles = np.array([[0, 200], [0, 255], [2200, 255], [2200, 200]])
+        below_subtitles = np.array(
+            [[0, 200], [0, 255], [2200, 255], [2200, 200]])
         # ensure white above and below text. Some blank space is needed for
         # tesseract
-        img = cv2.fillPoly(img, pts =[above_subtitles, below_subtitles], color=0)
+        img = cv2.fillPoly(
+            img, pts=[above_subtitles, below_subtitles], color=0)
         img = cv2.bitwise_not(img)
         img = cv2.GaussianBlur(img, (21, 21), 0)
         img = cv2.threshold(img, 200, 255, cv2.THRESH_BINARY)[1]
@@ -43,20 +46,35 @@ while(cap.isOpened()):
 
         textImage = Image.fromarray(img)
         hash = imagehash.average_hash(textImage, 32)
+        # only use tesseract if the subtitle changes. This is for performance
+        # and also to avoid having single frames of tesseract mistakes.
         if abs(prev_hash - hash) > 20:
             # Page segmentation mode (PSM) 13 means "Raw line. Treat the image
             # as a single text line, bypassing hacks that are
             # Tesseract-specific."
-            text = pytesseract.image_to_string(img, lang='chi_sim', config='--psm 13')
-            print(prev_hash - hash)
+            text = pytesseract.image_to_string(
+                img, lang='chi_sim', config='--psm 13')
             text = text.replace("-", "一")
             text = text.replace("+", "十")
-            text = text.replace("F", "上");
-            text = text.replace("，", "");
-            text = text.replace("。", "");
-            text = text.replace("”", "");
-            text = text.strip();
-            print(text)
+            text = text.replace("F", "上")
+            text = text.replace("，", "")
+            text = text.replace("。", "")
+            text = text.replace("”", "")
+            text = text.strip()
+
+            if prev_subtitle != text:
+                if prev_subtitle != "":
+                    line_start_time = millis_to_hhmmss(prev_change_millis)
+                    line_end_time = millis_to_hhmmss(
+                        cap.get(cv2.CAP_PROP_POS_MSEC))
+                    print(subtitle_index)
+                    print(line_start_time + " --> " + line_end_time)
+                    print(prev_subtitle)
+                    print()
+                    subtitle_index += 1
+                prev_subtitle = text
+                prev_change_millis = cap.get(cv2.CAP_PROP_POS_MSEC)
+
         prev_hash = hash
 
         # Press Q on keyboard to  exit
